4d3e6341412680b0ed2d0309859ccd8909127fc8
shellyfra <shellyfra@hotmail.com>
2021-06-07 00:07:18 +0300

    fix

--- Wet3/request.c
+++ Wet3/request.c
@@ -143,9 +143,9 @@ double elapsedTime = (con.start_req_arrival.tv_sec) * 1000.0; // sec to ms
    sprintf(buf, "%sStat-req-arrival: %f\r\n",buf, elapsedTime);
    sprintf(buf, "%sStat-req-dispatch: %f\r\n",buf, con.start_req_dispatch);
    sprintf(buf, "%sStat-thread-id: %d\r\n",buf, thread_statistics_p->thread_id);
-   sprintf(buf, "%sStat-thread-count: %s\r\n",buf, thread_statistics_p->thread_count);
-   sprintf(buf, "%sStat-thread-static: %s\r\n",buf, thread_statistics_p->thread_static);
-   sprintf(buf, "%sStat-thread-dynamic: %s\r\n\r\n",buf, thread_statistics_p->thread_dynamic);
+   sprintf(buf, "%sStat-thread-count: %u\r\n",buf, thread_statistics_p->thread_count);
+   sprintf(buf, "%sStat-thread-static: %u\r\n",buf, thread_statistics_p->thread_static);
+   sprintf(buf, "%sStat-thread-dynamic: %u\r\n\r\n",buf, thread_statistics_p->thread_dynamic);
    Rio_writen(fd, buf, strlen(buf));
 
    if (Fork() == 0) {

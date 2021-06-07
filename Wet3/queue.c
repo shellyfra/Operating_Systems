064ee071b9f064c0b677652b7e4f2e6a672c5f76
Shai Yehezkel <shai444@gmail.com>
2021-06-07 13:35:54 +0000

    Added double linked list implementation

--- Wet3/queue.c
+++ Wet3/queue.c
@@ -68,9 +68,15 @@ void enqueue(Queue* queue, Connection item , pthread_cond_t* condition ,pthread_
 
 void enqueue_drop_head(Queue* queue, Connection item , pthread_cond_t* condition ,pthread_mutex_t* mutex)
 {
-    
+    //TODO: Shai comment: Shouldn't this be just dequeue() and followed by enqueue(item) ?
+    //dequeue(queue,condition,mutex);
+    //enqueue(queue,item, condition,mutex);
+
+    // TODO: Shelly's code :
+    /*
     pthread_mutex_lock(mutex);
     // This is the critical part modyifing queue properties
+    
     queue->elements[queue->start].connfd = -1; // invalid connfd in order to check for errors if we accidentally try to run this element
     queue->start = (queue->start + 1) % queue->size; // drop the first element
 
@@ -79,8 +85,41 @@ void enqueue_drop_head(Queue* queue, Connection item , pthread_cond_t* condition
     //queue->element_count++; added and removed so number is the same
     pthread_cond_signal(condition);
     pthread_mutex_unlock(mutex);
+    */
+}
+void deleteNode(Queue* queue, node* to_delete)
+{
+    
+    if (queue->head == NULL || to_delete == NULL)
+        return;
+ 
+    /* If node to be deleted is head node */
+    if (queue->head == to_delete)
+    {
+        queue->head = to_delete->next;
+    }
+    if(queue->tail == to_delete)
+    {
+        queue->tail = to_delete->prev;
+    }
+ 
+    /* Change next only if node to be deleted is NOT the last node */
+    if (to_delete->next != NULL)
+    {
+        to_delete->next->prev = to_delete->prev;
+    }
+
+ 
+    /* Change prev only if node to be deleted is NOT the head node */
+    if (to_delete->prev != NULL)
+        to_delete->prev->next = to_delete->next;
+ 
+    /* Finally, free the memory occupied by del*/
+    free(to_delete);
+    queue->element_count--;
     
 }
+ 
 
 void enqueue_drop_random(Queue* queue, Connection item , pthread_cond_t* condition ,pthread_mutex_t* mutex)
 {
@@ -88,7 +127,7 @@ void enqueue_drop_random(Queue* queue, Connection item , pthread_cond_t* conditi
 
     pthread_mutex_lock(mutex);
     // This is the critical part modyifing queue properties
-    Connection * new_elements_array= (Connection*)malloc(sizeof(Connection)* queue->size);
+    //Connection * new_elements_array= (Connection*)malloc(sizeof(Connection)* queue->size);
     int drop_count = 0;
     double num_to_ceil = (double)queue->size/(double)4;
     double drop_num = ceil(num_to_ceil); // delete at least 1 element
@@ -106,29 +145,60 @@ void enqueue_drop_random(Queue* queue, Connection item , pthread_cond_t* conditi
     //          [0,1,5,3,4,2]
     //
     // Now 2 will never be generated again, as we made sure to make it out of bounds.
-    int * indexes_array = (int*) malloc(sizeof(int)*(queue->size));
+    int * indices_array = (int*) malloc(sizeof(int)*(queue->size));
+    int * indices_to_remove = (int*) malloc(sizeof(int)*(queue->size));
     for (unsigned int i = 0; i < queue->size; i++)
     {
-        indexes_array[i]=i;
+        indices_array[i]=i;
     }
-    
-
     while (drop_count < drop_num) { // choose 1/4 of the elements in the arrays
         int rand_index_in_array = rand() % (queue->size-drop_count); // random int between 0 and queue size
-        int rand_index = indexes_array[rand_index_in_array]; // Guaranteed to be an index that wasn't selected
+        int rand_index = indices_array[rand_index_in_array]; // Guaranteed to be an index that wasn't selected
+        indices_to_remove[rand_index]=1;
+        // Swap indexes in array:
+        int temp = indices_array[queue->size-drop_count];
+        indices_array[queue->size-drop_count] = rand_index;
+        indices_array[rand_index_in_array] = temp;
+
+        drop_count++;
+        
+    }
+    free(indices_array);
+    node* it = queue->head;
+    unsigned int count =0;
+    while (it)
+    {
+        if(indices_to_remove[count])
+        {
+            node* temp =it->next;
+            deleteNode(queue,it);
+            it=temp;
+        }
+        else
+        {
+            it = it->next;        
+        }        
+        count++;
+    }
+    free(indices_to_remove);
+
+    /*
+    while (drop_count < drop_num) { // choose 1/4 of the elements in the arrays
+        int rand_index_in_array = rand() % (queue->size-drop_count); // random int between 0 and queue size
+        int rand_index = indices_array[rand_index_in_array]; // Guaranteed to be an index that wasn't selected
         //if (queue->elements[rand_index].connfd != -1) { // id item wasn't already chosen
         queue->elements[rand_index].connfd = -1 ; // change to invalid value
 
         // Swap indexes in array:
-        int temp = indexes_array[queue->size-drop_count];
-        indexes_array[queue->size-drop_count] = rand_index;
-        indexes_array[rand_index_in_array] = temp;
+        int temp = indices_array[queue->size-drop_count];
+        indices_array[queue->size-drop_count] = rand_index;
+        indices_array[rand_index_in_array] = temp;
 
         drop_count++;
         //}
     }
-    free(indexes_array);
-
+  
+    
     int new_elements_index = 0;
     int old_queue_index = queue->start;
     for (int i = 0; i < queue->size; i++) { // add all un-chosen elements to the new array
@@ -149,7 +219,7 @@ void enqueue_drop_random(Queue* queue, Connection item , pthread_cond_t* conditi
     queue->end = new_elements_index; // if for example added 5 elements -> last one is array[4] and the new one is array[5]
     queue->elements[queue->end] = item;
     queue->element_count = new_elements_index +1; // how many added to the new list + the new element ( = 1 )
-
+*/
     pthread_cond_signal(condition);
     pthread_mutex_unlock(mutex);
 

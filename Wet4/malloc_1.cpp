d9fa4a34d286903b1be5462f824ed74b750758c3
Shai Yehezkel <shai444@gmail.com>
2021-06-21 18:18:33 +0000

    Added part 1 & 2, need to add new real size in struct

--- Wet4/malloc_1.cpp
+++ Wet4/malloc_1.cpp
@@ -9,9 +9,18 @@
         b. If ‘size’ is more than 10^8 , return NULL.
         c. If sbrk fails, return NULL.
 */
+const size_t SMALLOC_MAX_SIZE =100000000;
+const int SBRK_FAILURE = -1;
 void* smalloc(size_t size)
 {
-    
-    // TODO implement
-    return NULL;
+    if(size==0 || size > SMALLOC_MAX_SIZE)
+    {
+        return NULL;
+    }
+    void *block_ptr =sbrk(size);
+    if(*((int*)block_ptr)==SBRK_FAILURE)
+    {
+        return NULL;
+    }
+    return block_ptr;
 }
